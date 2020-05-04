55b4f9165d211e9437addba5e9b5afe177f72b5d
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-04 17:48:56 -0300

    Resolução 2º Exercício Módulo 01

---
+++ Módulo 01/02.js
@@ -0,0 +1,10 @@
+      /** 2º Exercício - Crie uma função que dado um intervalo (entre x e y) exiba todos número pares: */
+      function pares(x, y) {
+        for(i = x; i <= y; i++) {
+          if(i%2 === 0) {
+            console.log(i);
+          }
+        }
+      }
+
+      pares(32, 321);
