f5f2d0ea38ce67b77621746cabddfa1dfdffdc3f
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-05 17:19:26 -0300

    Finalizando App To-Do

--- Módulo 03/toDos.js
+++ Módulo 03/toDos.js
@@ -1,40 +1,61 @@
 /** Lista */
 var listElement = document.querySelector('#app ul');
 /** Input */
-var inputElemet = document.querySelector('#app input');
+var inputElement = document.querySelector('#app input');
 /** Botão */
 var buttonElement = document.querySelector('#app button');
 
-var toDos = [
-  "Fazer Café",
-  "Estudar JavaScript",
-  "Acessar Comunidade da Rocketseat"
-]
+var toDos = JSON.parse(localStorage.getItem('listToDos'));
 
-for (each of toDos) {
-  addLi(each);
-}
+function renderToDos() {
+  /** Limpar contúdo da lista HTML antes de renderizar, ou seja, limpar todo o conteúdo dentro de <ul></ul> */
+  listElement.innerHTML = '';
+  for(toDo of toDos) {
+    /** Criando elemento <li>toDo</li> */
+    var liElement = document.createElement('li');
+    var textLi = document.createTextNode(toDo);
+    liElement.appendChild(textLi);
+    /** Criando elemento <a href="#">Excluir</a> */
+    var linkElement = document.createElement('a');
+    var textLink = document.createTextNode('Excluir');
+    linkElement.setAttribute('href', '#');
+    linkElement.appendChild(textLink);
+    /** Adicionando elemento <a href="#">Excluir</a> dentro de <li>toDo</li> */
+    liElement.appendChild(linkElement);
 
-function addLi(text) {
-  var liElement = document.createElement('li');
-  var textLiElement = document.createTextNode(text);
-  liElement.appendChild(textLiElement);
+    /** Adicionando elemento completo em <ul></ul> */
+    listElement.appendChild(liElement);
 
-  var linkElement = document.createElement('a');
-  var textLinkElement = document.createTextNode('Excluir');
-  linkElement.setAttribute('href', '#');
-  linkElement.appendChild(textLinkElement);
-
-  linkElement.onclick = function() {
-    listElement.removeChild(liElement);
+    var pos = toDos.indexOf(toDo);
+    /** linkElement.onclick = removeToDo(pos);
+     *  Não é possivel usar este comando pois ao invés de atribuir estariamos executando a função
+     *  Por consequencia criando e já deletando o elemento
+     */
+    linkElement.setAttribute('onclick', 'removeToDo(' + pos + ')');
   }
-
-  liElement.appendChild(linkElement);
-
-  listElement.appendChild(liElement);
 }
 
-buttonElement.onclick = function() {
-  addLi(inputElemet.value);
-  inputElemet.value = '';
+renderToDos();
+
+function addToDo() {
+  var toDo = inputElement.value;
+  toDos.push(toDo);
+  inputElement.value = '';
+  renderToDos();
+  saveToStorage();
 }
+
+function removeToDo(pos) {
+  toDos.splice(pos, 1);
+  renderToDos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  /** localStorage() não tem habilidade para gravar vetores e objetos, somente string
+   *  Por isso é necessáio converter o nosso vetor em uma string utilizando JSON
+   */
+  localStorage.setItem('listToDos', JSON.stringify(toDos));
+}
+
+buttonElement.onclick = addToDo;
